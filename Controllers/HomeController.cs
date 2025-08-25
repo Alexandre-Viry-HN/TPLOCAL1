423e48cc7906a3fb9be77755312c472069b4d6d0
Alexandre-Viry-HN <aviry@hn-services.fr>
2025-08-25 10:32:54 +0200

    Premier push
    Création du projet, et ajout des fonctionnalités majeures.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using TPLOCAL1.Models;
+
+//Subject is find at the root of the project and the logo in the wwwroot/ressources folders of the solution
+//--------------------------------------------------------------------------------------
+//Careful, the MVC model is a so-called convention model instead of configuration,
+//The controller must imperatively be name with "Controller" at the end !!!
+namespace TPLOCAL1.Controllers
+{
+    public class HomeController(IWebHostEnvironment env) : Controller
+    {
+        //methode "naturally" call by router
+
+        private readonly IWebHostEnvironment _env = env;
+
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                return View();
+            else
+            {
+                //Call different pages, according to the id pass as parameter
+                switch (id)
+                {
+                    case "OpinionList":
+                        var opinionsPath = Path.Combine(_env.WebRootPath ?? "wwwroot", "XMLFile", "DataAvis.xml");
+                        var list = new OpinionList().GetAvis(opinionsPath);
+                        return View("OpinionList", list);
+                    case "Form":
+                        return View("Form", new FormModel());
+                    default:
+                        return View();
+                }
+            }
+        }
+
+
+        //methode to send datas from form to validation page
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ValidationFormulaire(FormModel model)
+        {
+            if (model.DateDebut.HasValue && model.DateDebut.Value >= DateTime.Today)
+            {
+                ModelState.AddModelError(nameof(model.DateDebut), "La date doit être antérieure au " + DateTime.Today.ToString("d") + ".");
+            }
+            if (string.IsNullOrWhiteSpace(model.Genre))
+                ModelState.AddModelError(nameof(model.Genre), "Sélectionnez un sexe.");
+            if (string.IsNullOrWhiteSpace(model.TypeFormation))
+                ModelState.AddModelError(nameof(model.TypeFormation), "Sélectionnez une formation.");
+
+            if (!ModelState.IsValid)
+                return View("Form", model);
+            return View("Validation", model);
+        }
+    }
+}
---
+++ Models/FormModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace TPLOCAL1.Models
+{
+    public class FormModel
+    {
+        [Required]
+        public string Nom { get; set; }
+
+        [Required]
+        public string Prenom { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public string Adresse { get; set; }
+
+        [Required, RegularExpression(@"^\d{5}$", ErrorMessage = "Code postal sur 5 chiffres.")]
+        public string CodePostal { get; set; }
+
+        [Required]
+        public string Ville { get; set; }
+
+        [Required, EmailAddress(ErrorMessage = "Email invalide.")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateDebut { get; set; }
+
+        [Required]
+        public string TypeFormation { get; set; }
+
+        public string AvisCobol { get; set; }
+        public string AvisCSharp { get; set; }
+    }
+}
